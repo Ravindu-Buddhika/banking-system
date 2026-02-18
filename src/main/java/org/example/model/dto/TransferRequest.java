ce5ed0f3769e59644afe86db98d27db4814f286d
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-02-18 19:43:18 +0530

    created TransferRequest

---
+++ src/main/java/org/example/model/dto/TransferRequest.java
@@ -0,0 +1,17 @@
+package org.example.model.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TransferRequest {
+    private String sender;
+    private String receiver;
+    private BigDecimal amount;
+    private String reference;
+}
