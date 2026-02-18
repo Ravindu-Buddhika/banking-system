9b9d9d59834018337018555f24e3c53cacb234a6
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-02-18 23:28:52 +0530

    added custom search methord to reposirtory

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.example.repository;
 import org.example.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
+    Optional<UserEntity> findByEmail(String email);
 }
